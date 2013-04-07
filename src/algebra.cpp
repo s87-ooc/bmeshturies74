768855a371836697447dea60ac63354ff22358e6
Charles Podkanski <charles@podkanski.com>
2013-04-07 09:38:57 +0200

    New algebra class

--- src/algebra.cpp
+++ src/algebra.cpp
@@ -18,709 +18,264 @@
 #include <cstring>
 #include <assert.h>
 
-// ----------------------------------------------------------------------------
 
-CVector::CVector()
-{	
-	m_Entries = 0;
-	m_Size = 0;
-}
-
-CVector::~CVector()
-{	
-	if (m_Entries > 0)
-	{
-		delete[] m_Entries;
-	}
-}
-
-CVector::CVector(uint size, double* data)
-{	
-	assert (size > 0);
-	
-	m_Size = size;
-	m_Entries = new double[m_Size];
-	
-	if (data)
-	{
-		// we're assuming that the data array has a proper size
-		for (uint i = 0; i < m_Size; i++)
-		{
-			m_Entries[i] = data[i];
-		}
-	}
-}
-
-CVector::CVector(const CVector& v)
+Vector::Vector(unsigned int size):
+vals_(size), col_vect(true)
 {
-	m_Size = v.size();
-	
-	m_Entries = new double[m_Size];
-	
-	for (uint i = 0; i < m_Size; i++)
-	{
-		m_Entries[i] = v[i];
-	}
 }
 
-uint CVector::size() const
+Vector::Vector(TVals &vals):
+vals_(vals), col_vect(true)
 {
-	return m_Size;
 }
 
-void CVector::zero()
+unsigned int Vector::size() const
 {
-	memset(m_Entries, 0, sizeof(double) * m_Size);
-}
-
-const double& CVector::operator[] (uint i) const
-{
-	assert(i < m_Size);
-	
-	return m_Entries[i];
-}
-
-double& CVector::operator[] (uint i)
-{
-	assert(i < m_Size);
-	
-	return m_Entries[i];
-}
-
-CVector CVector::operator- ()
-{
-	CVector v = *this;
-	
-	for (uint i = 0; i < m_Size; i++)
-	{
-		v[i] *= -1.0;
-	}
-	
-	return v;
-}
-
-CVector& CVector::operator+= (const CVector& v)
-{
-	assert(v.size() == m_Size);
-
-	for (uint i = 0; i < m_Size; i++)
-	{
-		m_Entries[i] += v[i];
-	}
-	
-	return *this;
-}
-
-CVector& CVector::operator-= (const CVector& v)
-{
-	assert(v.size() == m_Size);
-
-	for (uint i = 0; i < m_Size; i++)
-	{
-		m_Entries[i] -= v[i];
-	}
-	
-	return *this;
+	return (int)vals_.size();
 }
 
 
-CVector CVector::operator+ (const CVector& v)
+double Vector::norm2() const
 {
-	assert(v.size() == m_Size);
-
-	CVector vAdd = v;
+	double norm = 0;
 	
-	for (uint i = 0; i < m_Size; i++)
-	{
-		vAdd[i] += m_Entries[i];
-	}
+	for (int i = 0; i < vals_.size(); i++)
+		norm += pow(vals_[0], 2);
 	
-	return vAdd;
+	return sqrt(norm);
 }
 
-CVector CVector::operator- (const CVector& v)
+double Vector::operator()(unsigned int pos) const
 {
-	assert(v.size() == m_Size);
-
-	CVector vSub = *this;
-	
-	for (uint i = 0; i < m_Size; i++)
-	{
-		vSub[i] -= v[i];
-	}
-	
-	return vSub;
+	return vals_[pos];
 }
 
-CVector& CVector::operator*= (const double s)
+double& Vector::operator()(unsigned int pos)
 {
-	for (uint i = 0; i < m_Size; i++)
-	{
-		m_Entries[i] *= s;
-	}
-	
-	return *this;
+	return vals_[pos];
 }
 
-CVector operator* (const double s, const CVector& v)
+Vector Vector::operator*(Sparse const& M) const
 {
-	CVector vScaled = v;
-	for (uint i = 0; i < vScaled.size(); i++)
-	{
-		vScaled[i] *= s;
-	}
+	int n = (*this).size();
+	Vector res(n);
 	
-	return vScaled;
-}
-
-CVector operator* (const CVector& v, const double s)
-{
-	CVector vScaled = v;
-	for (uint i = 0; i < vScaled.size(); i++)
-	{
-		vScaled[i] *= s;
-	}
-	
-	return vScaled;
-}
-
-ostream& operator<<(ostream& o, const CVector& v)
-{
-	o << "[ ";
+    // implement multiplication
     
-	for (uint i = 0; i < v.size(); i++)
-	{
-		o << v[i] << " ";
-	}
 	
-	o << "] " << v.size();
-    return o;
-} 
-
-/*istream& operator>>(istream& i, CVector& v)
-{
-    float re,im;
-    i >> re >> im;
-    c = complexe(re,im);
-    return i;
-}*/
-
-// ----------------------------------------------------------------------------
-
-const double CMatrixData::s_Zero = 0.0;
-
-CMatrixDataDense::CMatrixDataDense()
-{
-	m_SizeRows = 0;
-	m_SizeColumns = 0;
-	m_Data = 0;
+	return res;
 }
 
-CMatrixDataDense::~CMatrixDataDense()
+Vector Vector::operator*(double s) const
 {
-	if (m_Data)
-	{
-		delete[] m_Data;
-	}
+    int n = (int)vals_.size();
+	Vector res(n);
+	
+	for (int i = 0; i < n; i++)
+		res(i) = s * vals_[i];
+	
+	return res;
 }
 
-CMatrixDataDense::CMatrixDataDense(uint rows, uint columns, double* data)
+Vector Vector::operator+(Vector const& v) const
 {
-	m_SizeRows = rows;
-	m_SizeColumns = columns;
-	m_Data = new double[m_SizeRows * m_SizeColumns];
+    int n = v.size();
+	Vector res(n);
 	
-	if (data)
-	{
-		for (uint i = 0; i < m_SizeRows * m_SizeColumns; i++)
-		{
-			m_Data[i] = data[i];
-		}
-	}
+	for (int i = 0; i < n; i++)
+		res(i) = vals_[i] + v(i);
+	
+	return res;
 }
 
-CMatrixDataDense::CMatrixDataDense(const CMatrixDataDense& mdata)
+
+Vector Vector::operator-(Vector const& v) const
 {
-	m_SizeRows = mdata.m_SizeRows;
-	m_SizeColumns = mdata.m_SizeColumns;
-	m_Data = new double[m_SizeRows * m_SizeColumns];
+    int n = v.size();
+	Vector res(n);
 	
-	for (uint i = 0; i < m_SizeRows * m_SizeColumns; i++)
-	{
-		m_Data[i] = mdata.m_Data[i];
-	}
+	for (int i = 0; i < n; i++)
+		res(i) = vals_[i] - v(i);
+	
+	return res;
 }
 
-/*double& CMatrixDataDense::operator() (uint r, uint c)
+Vector& Vector::operator+=(Vector &v)
 {
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	//cout << "nonconst ";
-	
-	return m_Data[m_SizeColumns * r + c];
-}
-
-const double& CMatrixDataDense::operator() (uint r, uint c) const
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	//cout << "const as rValue" << endl;
-	
-	return m_Data[m_SizeColumns * r + c];
-}*/
-
-// @ workaround
-double& CMatrixDataDense::_l(uint r, uint c)
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	return m_Data[m_SizeColumns * r + c];
-}
-
-const double& CMatrixDataDense::_r(uint r, uint c) const
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	return m_Data[m_SizeColumns * r + c];
-}
-// @@@
-
-// ----------------------------------------------------------------------------
-
-CMatrixDataDiag::CMatrixDataDiag()
-{
-	m_SizeRows = 0;
-	m_SizeColumns = 0;
-	m_Data = 0;
-}
-
-CMatrixDataDiag::~CMatrixDataDiag()
-{
-	if (m_Data)
-	{
-		delete[] m_Data;
-	}
-}
-
-CMatrixDataDiag::CMatrixDataDiag(uint size, double* data)
-{
-	m_SizeRows = m_SizeColumns = size;
-	m_Data = new double[size];
-	
-	if (data)
-	{
-		for (uint i = 0; i < m_SizeRows; i++)
-		{
-			m_Data[i] = data[i];
-		}
-	}
-}
-
-CMatrixDataDiag::CMatrixDataDiag(const CMatrixDataDiag& mdata)
-{
-	m_SizeRows = m_SizeColumns = mdata.m_SizeRows;
-	m_Data = new double[m_SizeRows];
-	
-	for (uint i = 0; i < m_SizeRows; i++)
-	{
-		m_Data[i] = mdata.m_Data[i];
-	}
-}
-
-/*double& CMatrixDataDiag::operator() (uint r, uint c)
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	//cout << "nonconst ";
-
-	return m_Data[r];
-}
-
-const double& CMatrixDataDiag::operator() (uint r, uint c) const
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	//cout << "const as rValue" << endl;
-	
-	if (r == c)
-	{
-		return m_Data[m_SizeColumns * r + c];
-	}
-}*/
-
-// @ workaround
-double& CMatrixDataDiag::_l(uint r, uint c)
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-
-	if (r != c)
-	{
-		cout << "lValue only possible on diagonal!" <<endl;
-		assert(false);
-	};
-	
-	return m_Data[r];
-}
-
-const double& CMatrixDataDiag::_r(uint r, uint c) const
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	if (r != c)
-	{
-		return CMatrixData::s_Zero;
-	}
-	else
-	{
-		return m_Data[r];
-	}
-}
-// @@@
-
-// ----------------------------------------------------------------------------
-
-CMatrixDataSparse::CMatrixDataSparse()
-{
-	m_SizeRows = 0;
-	m_SizeColumns = 0;
-	m_Data = 0;
-}
-
-CMatrixDataSparse::~CMatrixDataSparse()
-{
-	if (m_Data)
-	{
-		delete[] m_Data;
-	}
-}
-
-CMatrixDataSparse::CMatrixDataSparse(uint rows, uint columns)
-{
-	m_SizeRows = rows;
-	m_SizeColumns = columns;
-	
-	m_Data = 0;
-	// TODO
-}
-
-CMatrixDataSparse::CMatrixDataSparse(const CMatrixDataSparse& mdata)
-{
-	m_SizeRows = mdata.m_SizeRows;
-	m_SizeColumns = mdata.m_SizeColumns;
-	
-	m_Data = 0;
-	// TODO
-}
-
-/*double& CMatrixDataSparse::operator() (uint r, uint c)
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	//cout << "nonconst ";
-	
-	return m_Data[m_SizeColumns * r + c];
-}
-
-const double& CMatrixDataSparse::operator() (uint r, uint c) const
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	//cout << "const as rValue" << endl;
-	
-	return m_Data[m_SizeColumns * r + c];
-}*/
-
-// @ workaround
-double& CMatrixDataSparse::_l(uint r, uint c)
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	return m_Data[0];
-}
-
-const double& CMatrixDataSparse::_r(uint r, uint c) const
-{
-	assert (r < m_SizeRows);
-	assert (c < m_SizeColumns);
-	
-	return m_Data[0];
-}
-// @@@
-
-// ----------------------------------------------------------------------------
-
-CMatrix::CMatrix()
-{
-	m_Data = 0;
-	m_Type = eMDT_DENSE;
-}
-
-CMatrix::~CMatrix()
-{
-	if (m_Data)
-	{
-		delete m_Data;
-	}
-}
-
-CMatrix::CMatrix(uint rows, uint columns, eMatrixDataType type, double* data)
-{
-	assert(rows > 0);
-	assert(columns > 0);
-	
-	m_Type = type;
-	
-	if (m_Type == eMDT_DIAG && rows != columns)
-	{
-		cout << "Diagonal matrix has to be a square matrix!" << endl;
-		assert(false);
-	}
-	
-	switch(m_Type)
-	{
-		case eMDT_DENSE:
-			m_Data = new CMatrixDataDense(rows, columns, data);
-			break;
-		case eMDT_DIAG:
-			m_Data = new CMatrixDataDiag(rows, data);
-			break;
-		case eMDT_SPARSE:
-			m_Data = new CMatrixDataSparse(rows, columns);
-			break;
-		default:
-			cout << "Invalid matrix type!" << endl;
-			assert(false);
-			break;
-	}
-}
-
-CMatrix::CMatrix(const CMatrix& m)
-{
-	m_Type = m.m_Type;
-
-	if (m_Type == eMDT_DENSE)
-	{
-		CMatrixDataDense* pData = dynamic_cast<CMatrixDataDense*>(m.m_Data);
-		m_Data = new CMatrixDataDense(*pData);
-	}
-	else if (m_Type == eMDT_DIAG)
-	{
-		CMatrixDataDiag* pData = dynamic_cast<CMatrixDataDiag*>(m.m_Data);
-		m_Data = new CMatrixDataDiag(*pData);
-	}
-	else if (m_Type == eMDT_SPARSE)
-	{
-		CMatrixDataSparse* pData = dynamic_cast<CMatrixDataSparse*>(m.m_Data);
-		m_Data = new CMatrixDataSparse(*pData);
-	}
-	else
-	{
-		cout << "Invalid matrix type!" << endl;
-		assert(false);
-	}
-}
-
-uint CMatrix::sizeRows() const
-{
-	assert(m_Data);
-	
-	return m_Data->m_SizeRows;
-}
-
-uint CMatrix::sizeColumns() const
-{
-	assert(m_Data);
-	
-	return m_Data->m_SizeColumns;
-}
-
-/*double& CMatrix::operator() (uint r, uint c)
-{
-	assert(m_Data);
-	
-	return (*m_Data)(r, c);
-}
-
-const double& CMatrix::operator() (uint r, uint c) const
-{
-	assert(m_Data);
-	
-	return (*m_Data)(r, c);
-}*/
-
-// @ workaround
-double& CMatrix::_l(uint r, uint c)
-{
-	assert(m_Data);
-	
-	return m_Data->_l(r, c);
-}
-
-const double& CMatrix::_r(uint r, uint c) const
-{
-	assert(m_Data);
-	
-	return m_Data->_r(r, c);
-}
-// @@@
-
-CMatrix CMatrix::operator- ()
-{
-	assert (m_Data);
-	
-	CMatrix m(*this);
-	
-	for (uint i = 0; i < m.sizeRows(); i++)
-	{
-		for (uint j = 0; j < m.sizeColumns(); j++)
-		{
-			m._l(i, j) *= -1.0;
-		}
-	}
-	
-	return m;
-}
-
-CMatrix& CMatrix::operator+= (const CMatrix& m)
-{
-	assert (m_Data->m_SizeRows == m.sizeRows());
-	assert (m_Data->m_SizeColumns == m.sizeColumns());
-	
-	for (uint i = 0; i < m_Data->m_SizeRows; i++)
-	{
-		for (uint j = 0; j < m_Data->m_SizeColumns; j++)
-		{
-			m_Data->_l(i, j) += m.m_Data->_r(i, j);
-		}
-	}
-	
-	return *this;
-}
-
-CMatrix& CMatrix::operator-= (const CMatrix& m)
-{
-	assert (m_Data->m_SizeRows == m.sizeRows());
-	assert (m_Data->m_SizeColumns == m.sizeColumns());
-	
-	for (uint i = 0; i < m_Data->m_SizeRows; i++)
-	{
-		for (uint j = 0; j < m_Data->m_SizeColumns; j++)
-		{
-			m_Data->_l(i, j) -= m.m_Data->_r(i, j);
-		}
-	}
-	
-	return *this;
-}
-
-CMatrix CMatrix::operator+ (const CMatrix& m)
-{
-	assert (m_Data->m_SizeRows == m.sizeRows());
-	assert (m_Data->m_SizeColumns == m.sizeColumns());
-	
-	CMatrix mAdd(*this);
-	
-	for (uint i = 0; i < m_Data->m_SizeRows; i++)
-	{
-		for (uint j = 0; j < m_Data->m_SizeColumns; j++)
-		{
-			mAdd.m_Data->_l(i, j) += m.m_Data->_r(i, j);
-		}
-	}
-	
-	return mAdd;
-}
-
-CMatrix CMatrix::operator- (const CMatrix& m)
-{
-	assert (m_Data->m_SizeRows == m.sizeRows());
-	assert (m_Data->m_SizeColumns == m.sizeColumns());
-	
-	CMatrix mSub(*this);
-	
-	for (uint i = 0; i < m_Data->m_SizeRows; i++)
-	{
-		for (uint j = 0; j < m_Data->m_SizeColumns; j++)
-		{
-			mSub.m_Data->_l(i, j) -= m.m_Data->_r(i, j);
-		}
-	}
-	
-	return mSub;
-}
-
-CMatrix& CMatrix::operator*= (const double s)
-{
-	assert (m_Data);
-	
-	for (uint i = 0; i < m_Data->m_SizeRows; i++)
-	{
-		for (uint j = 0; j < m_Data->m_SizeColumns; j++)
-		{
-			m_Data->_l(i, j) *= s;
-		}
-	}
-	
-	return *this;
-}
-
-/*CMatrix operator* (const CVector& v, const CMatrix& m)
-{
-	assert(v.size() == m.sizeRows());
-
-	CMatrix mMult(m);
-	
-	for (uint i = 0; i < m_Data->m_SizeRows; i++)
-	{
-		for (uint j = 0; j < m_Data->m_SizeColumns; j++)
-		{
-			m.m_Data->_l(i, j) = 0.0;
-			for (uint k = 0; k < v.size(); k++)
-			{
-				m.m_Data->_l(i, j) += v[k] * m.m_Data->_l(k, j);
-			}
-		}
-	}
-	
-	return mMult;
-}*/
-
-/*
-CVector operator* (const CMatrix& m, const CVector& v);
-CMatrix operator* (const double s, const CMatrix& m);
-CMatrix operator* (const CMatrix& m, const double s);
-*/
-
-ostream& operator<<(ostream& o, const CMatrix& m)
-{
-	o << "[ ";
+	for (int i = 0; i < v.size(); i++)
+		vals_[i] += v(i);
     
-	for (uint i = 0; i < m.sizeRows(); i++)
-	{
-		for (uint j = 0; j < m.sizeColumns(); j++)
-		{
-			//o << m(i, j) << " ";
-			o << m._r(i, j) << " ";
-		}
-		
-		if (i < m.sizeRows() - 1)
-		{
-			cout << "; ";
-		}
-	}
+	return *this;
+}
+
+Vector& Vector::operator-=(Vector &v)
+{
+	for (int i = 0; i < v.size(); i++)
+		vals_[i] -= v(i);
 	
-	o << "] " << m.sizeRows() << "x" << m.sizeColumns() << " ";
+	return *this;
+}
+
+// ----------------------------------------------------------------------------
+
+ostream& operator<<(ostream &out, Vector& v)
+{
+	cout << "[ ";
+	for (int i = 0; i < v.size(); i++)
+	{
+		out << v(i) << " ";
+	}
+	cout << "] " << v.size();
+	
+	return out;
+}
+
+// ----------------------------------------------------------------------------
+
+
+Sparse::Sparse()
+{
+}
+
+Sparse::Sparse(unsigned int nrows, unsigned int ncols):
+nrows_(nrows), ncols_(ncols)
+{
+}
+
+unsigned int Sparse::ncols() const
+{
+    return ncols_;
+}
+
+unsigned int Sparse::nrows() const
+{
+    return nrows_;
+}
+
+unsigned int Sparse::nnz() const
+{
+    return nnz_;
+}
+
+istream& operator >>(istream &is, Sparse &M)
+{
+    is >> M.nrows_ >> M.ncols_ >> M.nnz_;
     
-	return o;
+    M.col_ind = TInd(M.nnz_);
+    M.row_ptr = TInd(M.nrows_+1, -1);
+    M.vals = TVals(M.nnz_);
+    
+    int r;
+    
+    for(int k = 0; k < M.nnz_; k++)
+    {
+        is >> r >> M.col_ind[k] >> M.vals[k];
+        if (M.row_ptr[r] < 0) { M.row_ptr[r] = k; }
+    }
+    
+    M.row_ptr[M.nnz_] = M.row_ptr[M.nnz_ - 1] + 1;
+    
+    return is;
+}
+
+ostream& operator <<(ostream &os, Sparse &M)
+{
+    return os;
+}
+
+
+double Sparse::operator()(unsigned int row, unsigned int col) const
+{
+    cout << "operator() const" << endl;
+    if (row > nrows_-1 || col > ncols_-1)
+    {
+        cout << "Erreur de dimension de la matrice" << endl;
+        return 0.0;
+    }
+    
+    for (int i = row_ptr[row]; i < row_ptr[row+1]; i++)
+    {
+        if ( col_ind[i] == col)
+            return vals[i];
+    }
+    
+    return 0.0;
+}
+
+double& Sparse::operator()(unsigned int row, unsigned int col)
+{
+    cout << "operator() non-const" << endl;
+    
+    if (row > nrows_-1 || col > ncols_-1)
+    {
+        cout << "Erreur de dimension de la matrice" << endl;
+        return vals[0];
+    }
+    
+    
+    return vals[0];
+}
+
+
+
+Vector Sparse::operator*(Vector const& x) const
+{
+    // dimension check
+    // if (ncols != v.size()) { // throw error }
+    
+    TVals y(ncols_);
+    for(int i=0; i<nrows_; i++)
+    {
+        y[i] = 0.0;
+        for(int k = row_ptr[i]; k < row_ptr[i+1]; k++)
+        {
+            y[i] += vals[k]*x(col_ind[k]);
+        }
+    }
+    return y;
+}
+
+Vector Sparse::jacobi(Vector const& b) const
+{
+    Vector x1(ncols_);
+    Vector x2(ncols_);
+    Vector& xcur = x1;
+    Vector& xnew = x2;
+    
+    int k = 0;
+    double diag;
+    bool convergence = false;
+    
+    while (!convergence)
+    {
+        for(int i=0; i<nrows_; i++)
+        {
+            xnew(i) = b(i);
+            diag = 0;
+            // iterate over the ith row of the matrix
+            for (k = row_ptr[i]; k < row_ptr[i+1]; k++)
+            {
+                if( col_ind[k] != i)
+                    xnew(i) -= vals[k]*xcur(col_ind[k]);
+                else
+                    diag = vals[k];
+            }
+            // do zero check
+            xnew(i) /= diag;
+        }
+        // TODO: convergence check
+        // implement equality operator for Vector
+        xcur = xnew;
+    }
+    
+    return xnew;
+    
+}
+
+
+
+Vector Sparse::gradient_conj(Vector const& b) const
+{
+    Vector p = b - (*this)*b;
+    return p;
 }
--- src/algebra.h
+++ src/algebra.h
@@ -16,235 +16,76 @@
 
 **************************************************************/
 
+
 #include <iostream>
+#include <vector>
+#include <math.h>
 
 using namespace std;
 
-typedef unsigned int uint;
+typedef vector<double> TVals;
+typedef vector<int> TInd;
 
-/** values with module below are treated as zero */
-const double ZERO_EPSILON = 0.00000001;
+class Sparse;
 
-// Note that depending on the constructor call the values aren't initialized
-
-class CVector
+class Vector
 {
 private:
-	double* m_Entries;
-	uint m_Size;
+	TVals vals_;
+    bool col_vect;
 	
 public:
-	CVector();
-	~CVector();
+	Vector();
+    Vector(unsigned int);
+	Vector(TVals &vals);
+    
+	unsigned int size() const;
+	double norm2() const;
 	
-	/** creates a vector with "size" entries, filled with data if it's not 0 */
-	CVector(uint size, double* data = 0);
+	double operator()(unsigned int pos) const;
+	double& operator()(unsigned int pos);
 	
-	/** copy constructor */
-	CVector(const CVector& v);
+	Vector operator*(Sparse const& m) const;
+	Vector operator*(double s) const;
+	Vector operator+(Vector const& v) const;
+	Vector operator-(Vector const& v) const;
 	
-	/** returns the number of entries */
-	uint size() const;
-	
-	/** set all components to 0 */
-	void zero();
-
-	/** get the i-th component */
-	const double& operator[] (uint i) const;
-	
-	/** access the i-th component for writing */
-	double& operator[] (uint i);
-	
-	/** unary operators */
-	CVector operator- ();
-	
-	/** vector addition, etc */
-	CVector& operator+= (const CVector& v);
-	CVector& operator-= (const CVector& v);
-	CVector operator+ (const CVector& v);
-	CVector operator- (const CVector& v);
-	
-	/** scalar multiplication */
-	CVector& operator*= (const double s);
+	Vector& operator+=(Vector& v);
+	Vector& operator-=(Vector& v);
 };
 
-/** scalar multiplications */
-CVector operator* (const double s, const CVector& v);
-CVector operator* (const CVector& v, const double s);
+ostream& operator<<(ostream &out, Vector& v);
 
-// operations with matrices defined below
 
-ostream& operator<<(ostream& o, const CVector& v);
-//istream& operator>>(istream& i, CVector& v);
-
-// ----------------------------------------------------------------------------
-
-// how data is stored in a matrix depends on what type of matrix we have
-
-class CMatrixData
-{
-friend class CMatrix;
-protected:
-	uint m_SizeRows;
-	uint m_SizeColumns;
-
-public:
-	static const double s_Zero;
-
-	/** access the (r, c) component non-const (rValue/lValue) */
-	//virtual double& operator() (uint r, uint c) = 0;
-
-	/** access the (r, c) component const (rValue only) */
-	//virtual const double& operator() (uint r, uint c) const = 0;
-	
-	// @ workaround
-	virtual double& _l(uint r, uint c) = 0;
-	virtual const double& _r(uint r, uint c) const = 0;
-	// @@@
-};
-
-// ----------------------------------------------------------------------------
-
-class CMatrixDataDense : public CMatrixData
-{
+class Sparse {
 private:
-	double* m_Data;
-
+    TVals vals;
+    TInd col_ind, row_ptr;
+    
+    unsigned int nrows_, ncols_, nnz_;
+    
+    friend istream& operator >>(istream&, Sparse&);
+    friend ostream& operator <<(ostream&, Sparse&);
+    
 public:
-	CMatrixDataDense();
-	~CMatrixDataDense();
-
-	CMatrixDataDense(uint rows, uint columns, double* data = 0);
-
-	/** copy constructor */
-	CMatrixDataDense(const CMatrixDataDense& mdata);
-	
-	//double& operator() (uint r, uint c);
-	//const double& operator() (uint r, uint c) const;
-	
-	// @ workaround
-	double& _l(uint r, uint c);
-	const double& _r(uint r, uint c) const;
-	// @@@
+    Sparse();
+    Sparse(unsigned int nrows, unsigned int ncols);
+    
+    unsigned int ncols() const;
+    unsigned int nrows() const;
+    unsigned int nnz() const;
+    
+    
+    double operator()(unsigned int, unsigned int) const;
+    double& operator()(unsigned int, unsigned int);
+    
+    Vector operator*(Vector const&) const;
+    
+    Vector jacobi(Vector const&) const;
+    Vector gradient_conj(Vector const&) const;
+    
+    
 };
 
-// ----------------------------------------------------------------------------
-
-class CMatrixDataDiag : public CMatrixData
-{
-private:
-	double* m_Data;
-
-public:
-	CMatrixDataDiag();
-	~CMatrixDataDiag();
-
-	CMatrixDataDiag(uint size, double* data = 0);
-
-	/** copy constructor */
-	CMatrixDataDiag(const CMatrixDataDiag& mdata);
-	
-	//double& operator() (uint r, uint c);
-	//const double& operator() (uint r, uint c) const;
-	
-	// @ workaround
-	double& _l(uint r, uint c);
-	const double& _r(uint r, uint c) const;
-	// @@@
-};
-
-// ----------------------------------------------------------------------------
-
-class CMatrixDataSparse : public CMatrixData
-{
-private:
-	double* m_Data;
-
-public:
-	CMatrixDataSparse();
-	~CMatrixDataSparse();
-
-	CMatrixDataSparse(uint rows, uint columns);
-	
-	/** copy constructor */
-	CMatrixDataSparse(const CMatrixDataSparse& mdata);
-	
-	//double& operator() (uint r, uint c);
-	//const double& operator() (uint r, uint c) const;
-	
-	// @ workaround
-	double& _l(uint r, uint c);
-	const double& _r(uint r, uint c) const;
-	// @@@
-};
-
-// ----------------------------------------------------------------------------
-
-enum eMatrixDataType
-{
-	eMDT_DENSE = 0,
-	eMDT_DIAG,
-	eMDT_SPARSE
-};
-
-// Note that depending on the matrix data type the entries aren't initialized
-
-class CMatrix
-{
-private:
-	/** data depends on the type of the matrix */
-	CMatrixData* m_Data;
-	
-	/** stores the type of the matrix */
-	eMatrixDataType m_Type;
-	
-public:
-	CMatrix();
-	~CMatrix();
-	
-	/** general constructor for matrices, data is only used for DENSE and DIAG matrices  */
-	CMatrix(uint rows, uint columns, eMatrixDataType type = eMDT_DENSE, double* data = 0);
-	
-	/** copy constructor */
-	CMatrix(const CMatrix& m);
-	
-	/** returns the number of rows */
-	uint sizeRows() const;
-	
-	/** returns the number of columns */
-	uint sizeColumns() const;
-
-	//double& operator() (uint r, uint c);	
-	//const double& operator() (uint r, uint c) const;
-	
-	// @ workaround
-	double& _l(uint r, uint c);
-	const double& _r(uint r, uint c) const;
-	// @@@
-	
-	/** unary operators */
-	CMatrix operator- ();
-	
-	/** matrix addition, etc */
-	CMatrix& operator+= (const CMatrix& m);
-	CMatrix& operator-= (const CMatrix& m);
-	CMatrix operator+ (const CMatrix& m);
-	CMatrix operator- (const CMatrix& m);
-	
-	/** multiplication with scalar */
-	CMatrix& operator *= (const double s);
-};
-
-/** multiplication with vector from left gives vector */
-CVector operator* (const CVector& v, const CMatrix& m);
-/** multiplication with vector from right gives vector */
-CVector operator* (const CMatrix& m, const CVector& v);
-
-/** multiplication with scalar from left */
-CMatrix operator* (const double s, const CMatrix& m);
-/** multiplication with scalar from right */
-CMatrix operator* (const CMatrix& m, const double s);
-
-ostream& operator<<(ostream& o, const CMatrix& m);
 
 #endif // __ALGEBRA_H__
