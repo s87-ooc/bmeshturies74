2685bc6328f6970a09f60a967a4e79922b35f75d
s87-ooc <s87.ooc@gmail.com>
2013-04-07 10:18:19 +0200

    defined Algebra data structures, etc

--- src/algebra.h
+++ src/algebra.h
@@ -26,65 +26,79 @@ using namespace std;
 typedef vector<double> TVals;
 typedef vector<int> TInd;
 
+// ----------------------------------------------------------------------------
+
 class Sparse;
 
 class Vector
 {
 private:
-	TVals vals_;
-    bool col_vect;
+	TVals mVals;
+
+	friend istream& operator>> (istream &in, Vector& v);
+	friend ostream& operator<< (ostream &out, Vector& v);
 	
 public:
 	Vector();
-    Vector(unsigned int);
+    Vector(unsigned int size);
 	Vector(TVals &vals);
     
 	unsigned int size() const;
+
+	/** Euclidean norm */
 	double norm2() const;
+
+	double operator() (unsigned int pos) const;
+	double& operator() (unsigned int pos);
 	
-	double operator()(unsigned int pos) const;
-	double& operator()(unsigned int pos);
+	Vector operator+ (Vector const& v) const;
+	Vector operator- (Vector const& v) const;
 	
-	Vector operator*(Sparse const& m) const;
-	Vector operator*(double s) const;
-	Vector operator+(Vector const& v) const;
-	Vector operator-(Vector const& v) const;
-	
-	Vector& operator+=(Vector& v);
-	Vector& operator-=(Vector& v);
+	Vector& operator+= (Vector& v);
+	Vector& operator-= (Vector& v);
+
+	Vector& operator*= (const double s);
 };
 
-ostream& operator<<(ostream &out, Vector& v);
+Vector operator* (const Vector& v, const double s);
+Vector operator* (const double s, const Vector& v);
 
+/// dot product v1 * v2
+double dot(const Vector& v1, const Vector& v2);
 
-class Sparse {
+// ----------------------------------------------------------------------------
+
+class Sparse
+{
 private:
-    TVals vals;
-    TInd col_ind, row_ptr;
-    
-    unsigned int nrows_, ncols_, nnz_;
+    TVals mVals;
+    TInd mColInd, mRowPtr;
     
     friend istream& operator >>(istream&, Sparse&);
     friend ostream& operator <<(ostream&, Sparse&);
+
+	friend Vector operator* (const Vector& v, const Sparse& m);
+	friend Vector operator* (const Sparse& m, const Vector& v);
+
+	/// x' * m * x
+	friend double prod (const Vector& v, const Sparse& m);
     
 public:
     Sparse();
+    Sparse(const Sparse& m);
+
+    Sparse(const TVals& vals, const TInd& colInd, const TInd& rowPtr);
     Sparse(unsigned int nrows, unsigned int ncols);
     
-    unsigned int ncols() const;
-    unsigned int nrows() const;
-    unsigned int nnz() const;
+    unsigned int sizeColumns() const;
+    unsigned int sizeRows() const;
+    unsigned int sizeNNZ() const;
     
+    double operator() (unsigned int col, unsigned int row) const;
+    double& operator() (unsigned int col, unsigned int row);
     
-    double operator()(unsigned int, unsigned int) const;
-    double& operator()(unsigned int, unsigned int);
-    
-    Vector operator*(Vector const&) const;
-    
-    Vector jacobi(Vector const&) const;
-    Vector gradient_conj(Vector const&) const;
-    
-    
+    Vector jacobi(Vector const& v) const;
+    Vector gradient_conj(Vector const& v) const;
 };
 
 
